f7af861fd84692453da134bbc627613c1a9d601e
rbaron <rbaron@rbaron.net>
2020-12-14 10:32:00 +0100

    day14 cleanup

--- day14/main.py
+++ day14/main.py
@@ -1,14 +1,18 @@
 import fileinput
+import functools
 import re
 from collections import defaultdict
+
+MEM_PATT = re.compile('mem\[(\d+)] = (\d+)')
 
 
 def apply_mask(mask, n):
     res = int(n)
-    for i, char in enumerate(reversed(mask)):
-        if char == '1':
+    for i in range(len(mask)):
+        pos = len(mask) - 1 - i
+        if mask[pos] == '1':
             res |= (1 << i)
-        elif char == '0':
+        elif mask[pos ]== '0':
             res &= ~(1 << i)
     return res
 
@@ -21,41 +25,33 @@ def part1(arg):
             mask = arg1
         elif cmd == 'mem':
             mem[arg1] = apply_mask(mask, arg2)
-
     return sum(mem.values())
 
 
+@functools.lru_cache(maxsize=None)
 def combinations(part):
     if not part:
         return ['']
 
     if part[0] != 'X':
-        return [
-            part[0] + c for c in combinations(part[1:])
-        ]
+        return [part[0] + c for c in combinations(part[1:])]
     else:
-        return [
-            prefix + comb
-            for prefix in '01'
-            for comb in combinations(part[1:])
-        ]
+        return [prefix + comb
+                for prefix in '01'
+                for comb in combinations(part[1:])
+                ]
 
 
 def addresses(mask, addr_in):
-    addr = list(reversed(format(int(addr_in), '#038b')[2:]))
-    res = ''
-    for i, char in enumerate(reversed(mask)):
-        if char == '1':
-            res += '1'
-        elif char == '0':
-            res += addr[i]
-        else:
-            res += 'X'
-
-    res = list(reversed(res))
-    print('res', res)
-
-    return combinations(res)
+    '''Returns a list of addresses given a mask and a base address.'''
+    # Convert address from base 10 to base 2. Strip the leading '0b'.
+    addr = format(int(addr_in), '#038b')[2:]
+    res = (
+        a if m == '0' else m
+        for a, m in zip(addr, mask)
+    )
+    # Convers to tuple so it's hashable and cacheable.
+    return combinations(tuple(res))
 
 
 def part2(arg):
@@ -67,19 +63,14 @@ def part2(arg):
         elif cmd == 'mem':
             for addr in addresses(mask, arg1):
                 mem[addr] = int(arg2)
-
-    print(mem.values())
     return sum(mem.values())
-
-
-mem_patt = re.compile('mem\[(\d+)] = (\d+)')
 
 
 def parse(line):
     if line.startswith('mask'):
         return ('mask', line.split()[-1], 0)
     else:
-        m = re.match(mem_patt, line)
+        m = re.match(MEM_PATT, line)
         return ('mem', m.group(1), m.group(2))
 
 
